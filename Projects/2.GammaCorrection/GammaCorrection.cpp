314d6fc15ddc1f7d056f4753643e113af1d35a2b
seal2002 <phannhuthai@gmail.com>
2017-10-12 16:29:39 +0700

    Added Gamma Correction Demo

---
+++ Projects/2.GammaCorrection/GammaCorrection.cpp
@@ -0,0 +1,143 @@
+#include <MainWindow.hpp>
+#include <glm\glm.hpp>
+#include <glm\common.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+#include <GLFW\include\GLFW\glfw3.h>
+#include <glad\glad.h>
+
+#include "common\Shader.h"
+#include "common\Camera.h"
+#include "common\LoadTexture.h"
+
+#define PATH "..\\Projects\\2.GammaCorrection"
+
+using namespace OpenGLWindow;
+
+void do_movement();
+unsigned int loadTexture(const char *path);
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+bool* keys;
+bool gamma = false;
+
+static int SCR_W = 1280;
+static int SCR_H = 720;
+
+float groundVertices[] = {
+    // positions            // normals         // texcoords
+    10.0f, -0.5f,  10.0f,  0.0f, 1.0f, 0.0f,  10.0f,  0.0f,
+    -10.0f, -0.5f,  10.0f,  0.0f, 1.0f, 0.0f,   0.0f,  0.0f,
+    -10.0f, -0.5f, -10.0f,  0.0f, 1.0f, 0.0f,   0.0f, 10.0f,
+
+    10.0f, -0.5f,  10.0f,  0.0f, 1.0f, 0.0f,  10.0f,  0.0f,
+    -10.0f, -0.5f, -10.0f,  0.0f, 1.0f, 0.0f,   0.0f, 10.0f,
+    10.0f, -0.5f, -10.0f,  0.0f, 1.0f, 0.0f,  10.0f, 10.0f
+};
+
+int main()
+{
+    Window window(SCR_W, SCR_H, "Gamma Correction");
+
+    glEnable(GL_DEPTH_TEST);
+    
+    string path(PATH);
+	path += "\\GammaCorrection";
+	Shader shader(path.c_str());
+    
+    unsigned int groundVAO, groundVBO;
+    glGenBuffers(1, &groundVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, groundVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(groundVertices), groundVertices, GL_STATIC_DRAW);
+    glGenVertexArrays(1, &groundVAO);
+    glBindVertexArray(groundVAO);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (void*)(3 * sizeof(GL_FLOAT)));
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (void*)(6 * sizeof(GL_FLOAT)));
+
+    // load textures
+    // -------------
+    unsigned int floorTexture = loadTexture("..\\Resources\\wood.png");
+    unsigned int floorTextureGammaCorrected = loadTexture("..\\Resources\\wood.png", true);
+
+	// lighting info
+	// -------------
+	glm::vec3 lightPositions[] = {
+		glm::vec3(-3.0f, 0.0f, 0.0f),
+		glm::vec3(-1.0f, 0.0f, 0.0f),
+		glm::vec3(1.0f, 0.0f, 0.0f),
+		glm::vec3(3.0f, 0.0f, 0.0f)
+	};
+	glm::vec3 lightColors[] = {
+		glm::vec3(0.25),
+		glm::vec3(0.50),
+		glm::vec3(0.75),
+		glm::vec3(1.00)
+	};
+
+    while (!window.shouldClose())
+    {
+        keys = window.getKeyPress();
+        do_movement();
+
+		// render
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+		shader.Use();
+		glm::mat4 projection = glm::perspective(45.0f, (float)SCR_W / (float)SCR_H, 0.1f, 100.0f);
+		glm::mat4 view = camera.GetViewMatrix();
+		shader.setMat4("projection", projection);
+		shader.setMat4("view", view);
+		// set light uniforms
+		glUniform3fv(glGetUniformLocation(shader.Program, "lightPositions"), 4, &lightPositions[0][0]);
+		glUniform3fv(glGetUniformLocation(shader.Program, "lightColors"), 4, &lightColors[0][0]);
+		shader.setVec3("viewPos", camera.cameraPos);
+		shader.setInt("gamma", gamma);
+		// floor
+		glBindVertexArray(groundVAO);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, gamma ? floorTextureGammaCorrected : floorTexture);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        window.swapBuffers();
+        window.pollEvents();
+    }
+}
+
+void do_movement()
+{
+    if (keys[GLFW_KEY_W])
+    {
+        camera.ProcessKeyboard(Camera_Movement::FORWARD);
+    }
+    if (keys[GLFW_KEY_S])
+    {
+        camera.ProcessKeyboard(Camera_Movement::BACKWARD);
+    }
+    if (keys[GLFW_KEY_A])
+    {
+        camera.ProcessKeyboard(Camera_Movement::LEFT);
+    }
+    if (keys[GLFW_KEY_D])
+    {
+        camera.ProcessKeyboard(Camera_Movement::RIGHT);
+    }
+    if (keys[GLFW_KEY_LEFT])
+    {
+        camera.ProcessKeyboard(Camera_Movement::ROTATE_LEFT);
+    }
+    if (keys[GLFW_KEY_RIGHT])
+    {
+        camera.ProcessKeyboard(Camera_Movement::ROTATE_RIGHT);
+    }
+    if (keys[GLFW_KEY_UP])
+    {
+        camera.ProcessKeyboard(Camera_Movement::ROTATE_UP);
+    }
+    if (keys[GLFW_KEY_DOWN])
+    {
+        camera.ProcessKeyboard(Camera_Movement::ROTATE_DOWN);
+    }
+}
