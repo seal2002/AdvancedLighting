c181dbce6c44b1985c02ae5d6362348758b18621
seal2002 <phannhuthai@gmail.com>
2018-01-03 16:58:58 +0700

    Fix wrong light source

--- Projects/3.2.ShadowMappingBase/ShadowMapping.fs
+++ Projects/3.2.ShadowMappingBase/ShadowMapping.fs
@@ -34,9 +34,9 @@ void main()
 {
     vec3 color = texture(diffuseTexture, fs_in.TexCoords).rgb;
     vec3 normal = normalize(fs_in.Normal);
-    vec3 lightColor = vec3(1.0);
+    vec3 lightColor = vec3(0.3);
     // ambient
-    vec3 ambient = 0.15 * color;
+    vec3 ambient = 0.3 * color;
     // diffuse
     vec3 lightDir = normalize(lightPos - fs_in.FragPos);
     float diff = max(dot(lightDir, normal), 0.0);
@@ -46,10 +46,10 @@ void main()
     float spec = 0.0;
     vec3 halfwayDir = normalize(lightDir + viewDir);  
     spec = pow(max(dot(normal, halfwayDir), 0.0), 64.0);
-    vec3 specular = spec * lightColor;    
+    vec3 specular = spec * lightColor;
     // calculate shadow
-    float shadow = ShadowCalculation(fs_in.FragPosLightSpace);       
+    float shadow = ShadowCalculation(fs_in.FragPosLightSpace);
     vec3 lighting = (ambient + (1.0 - shadow) * (diffuse + specular)) * color;    
 
-    FragColor = vec4(lighting, 1.0);
+    FragColor = vec4(lighting, 1.0f);
 }
